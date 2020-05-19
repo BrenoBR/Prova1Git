56696e550d7431e19acdde734a3f01620516bb3b
BrenoBR <breno-hp2@hotmail.com>
2020-05-19 07:54:06 -0300

    Leitura dos outros dados

--- main.cpp
+++ main.cpp
@@ -4,14 +4,20 @@ using namespace std;
 
 int main()
 {
-    int ticketmedio;
+    float ticketmedio;
     int qtcompras;
+    int atrasado;
+    char metodo;
     cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
     cout << "--------------------------------"<< endl;
     cout << "INFORME OS DADOS DO ULTIMO ANO" << endl << endl;
     cout << "Quantas compras o cliente fez no ultimo ano?";
     cin >> qtcompras;
-    cout << "Qual o ticket medio?";
+    cout << "Qual o ticket medio? ";
     cin >> ticketmedio;
+    cout << "Quantas vezes o cliente atrasou o pagamento? " ;
+    cin >> atrasado;
+    cout << "A maioria das compras foi em dinheiro, cartao ou boleto (D/C/B)? ";
+    cin >> metodo;
 
 }

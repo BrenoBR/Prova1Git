e9604f5f97218b05fc8c3f13a98b76dc9aa5c3a3
BrenoBR <breno-hp2@hotmail.com>
2020-05-19 08:05:48 -0300

    Scores de volume de compras

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main()
     int qtcompras;
     int atrasado;
     char metodo;
+    int scorecompras;
     cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
     cout << "--------------------------------"<< endl;
     cout << "INFORME OS DADOS DO ULTIMO ANO" << endl << endl;
@@ -19,5 +20,19 @@ int main()
     cin >> atrasado;
     cout << "A maioria das compras foi em dinheiro, cartao ou boleto (D/C/B)? ";
     cin >> metodo;
+    cout << endl;
+    if (ticketmedio <= 3000 && qtcompras <= 2){
+        scorecompras = 20;
+    }
+    else{
+        if (ticketmedio <=3000 && qtcompras >=2){
+            scorecompras = 40;
+        }
+        else{
+            scorecompras = 60;
+        }
+    }
+    cout << "Score de volume de compras = " << scorecompras <<  " pontos " << endl << endl;
 
 }
+
